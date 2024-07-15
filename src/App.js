de7c2faad6b81d3933fd0b9de9511c54c998ca52
SarvaniK-26 <sarvani.kunapareddy3@gmail.com>
2024-07-14 19:42:37 -0500

    setting up intro page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Link, Element } from 'react-scroll';
 import './App.css';
+import DetailsTile from './components/DetailsTile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <div className="nav-links">
+          <Link to="about" smooth={true} duration={500}>about</Link>
+          <Link to="experience" smooth={true} duration={500}>experience</Link>
+          <Link to="projects" smooth={true} duration={500}>projects</Link>
+          <Link to="passion" smooth={true} duration={500}>passions</Link>
+        </div>
+      </nav>
+
+      <div className="introduction-container">
+        <h2 className="introduction">Hi, my name is</h2>
+        <h1 className='name'>Sarvani Kunapareddy</h1>
+      </div>
+
+      <div className="content">
+        <Element name="about" className="element">
+        </Element>
+      </div>
+
     </div>
   );
 }
---
+++ src/components/DetailsTile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './DetailsTile.css'
+
+const DetailsTile = ({language}) => {
+    return (
+        <div>
+            <span>{language}</span>
+        </div>
+    );
+};
+
+export default DetailsTile;
