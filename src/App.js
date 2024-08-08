bb02520acc987c90dd16fe4f2229768571064239
SarvaniK-26 <sarvani.kunapareddy3@gmail.com>
2024-08-07 23:54:35 -0500

    added passions and tabs for all sections

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { Link, Element } from 'react-scroll';
 import './App.css';
 import DetailsTile from './components/DetailsTile';
 import picture from '/Users/sarvk/Desktop/sarvsite/src/pictures/alaskamainpic.jpg'; 
+import thdpic from '/Users/sarvk/Desktop/sarvsite/src/pictures/thdpic.png'; 
+
 
 function App() {
   return (
     <div className="App">
       <nav>
         <div className="nav-links">
-          <Link to="aboutintro-container" smooth={true} duration={500}>about</Link>
-          <Link to="experience" smooth={true} duration={500}>experience</Link>
-          <Link to="projects" smooth={true} duration={500}>projects</Link>
-          <Link to="passion" smooth={true} duration={500}>passions</Link>
+          <Link to="aboutintro-container" smooth={true} duration={500} offset={-100}>about</Link>
+          <Link to="experience-container" smooth={true} duration={500} offset={-100}>experience</Link>
+          <Link to="project-container" smooth={true} duration={500} offset={-100}>projects</Link>
+          <Link to="passion-container" smooth={true} duration={500} offset={-100}>passions</Link>
         </div>
       </nav>
 
@@ -22,14 +24,12 @@ function App() {
         <h3 className='details'>trying to learn, travel, and have a good time </h3>
       </div>
 
-      <div className="aboutintro-container">
+      <Element name="aboutintro-container" className="aboutintro-container">
         <h2 className="introduction">a little about me :)</h2>
-      </div>
-
+      </Element>
 
       <div className='aboutme-div'>
-
-        <body className="aboutme-vert-container">
+        <div className="aboutme-vert-container">
             <h3 className='details'>
             Stumbled into the world of computer science in high school after learning that I could turn data into stories and insights with just a few lines of code.</h3>
 
@@ -38,24 +38,30 @@ function App() {
 
             <h3 className='details'>
             I’ll be continuing my studies at the University of California at San Diego studying Computer Science and specializing in Artificial Intelligence. </h3>
-        </body>
-
-              <img src={picture} className='aboutme-pic' />;
-
-      </div>
-      
-      
-
-      
-
-
-
-      <div className="content">
-        <Element name="about" className="element">
-        </Element>
+        </div>
+        <img src={picture} className='aboutme-pic' alt="About Me" />
       </div>
 
+      <div className="experience-container">
+        <h2 className="introduction">experiences</h2>
+      </div>
+
+      <div className="project-container">
+        <h2 className="introduction">some cool things i tried to build</h2>
+      </div>
+       
+      <div className="passion-container">
+        <h2 className="introduction">what i’m up to when i’m not staring at my laptop coding</h2>
+      </div>
+
+      <div className='passions'>
+        <div className = 'passions-row'>
+          <h3 className='details'>The Hidden Dream</h3>
+        </div>
+          <img src={thdpic} className='thdpic' alt="THD" />
+        </div>
     </div>
+
   );
 }
 
