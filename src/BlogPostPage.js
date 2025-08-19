893e80f28d669cd851dac36f2d32d393d83a84a6
Sarvani Kunapareddy <67604941+SarvaniK-26@users.noreply.github.com>
2025-08-19 00:01:17 -0500

    updates

--- src/BlogPostPage.js
+++ src/BlogPostPage.js
@@ -22,7 +22,7 @@ export default function BlogPostPage() {
 
       <div className="top-nav">
         <Link to="/">
-          <button className="button">Return to Home</button>
+          <button className="button">Home</button>
         </Link>
       </div>
 
@@ -31,7 +31,7 @@ export default function BlogPostPage() {
       </div>
 
       <Link to="/blog">
-        <button className="button">Return to More Blogs</button>
+        <button className="button">More Blogs</button>
       </Link>
     </div>
   );
